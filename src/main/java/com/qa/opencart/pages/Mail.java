c2f553b749af745c707e8489fc06bbf14b04ca87
unknown <pichika.ganapathi@Mphasis.com>
2024-04-03 10:43:51 +0530

    added naveen code

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+	public void sendMail() {
+		System.out.println("mail sent ----naveen");
+	}
+	
+	public void getMail() {
+		System.out.println("mail got----naveen");
+	}
+
+}
